59b295cc92e648ae64c3b2e3bcdae1e5a0d64e28
Intersteller_Jasper <93512741+interstellarH@users.noreply.github.com>
2022-06-19 23:27:43 +0800

    Create auxiliary_function.h

---
+++ YJY/auxiliary_function.h
@@ -0,0 +1,18 @@
+#ifndef AUXILIARY_FUNCTION_H
+#define AUXILIARY_FUNCTION_H
+
+#endif // AUXILIARY_FUNCTION_H
+#include <QPoint>
+#include <cmath>
+
+inline bool collisionWithCircle(QPoint p1,int r1,QPoint p2,int r2)
+{//在h文件内部实现函数
+    int xx=p1.x()-p2.x();
+    int yy=p1.y()-p2.y();
+    int dis=sqrt(xx*xx+yy*yy);
+    if(dis<r1+r2)
+    {
+        return true;
+    }
+    return false;
+}
