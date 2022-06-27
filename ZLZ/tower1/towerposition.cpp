284b4a9325d4dcabf231cc72ad622a35169906c7
Intersteller_Jasper <93512741+interstellarH@users.noreply.github.com>
2022-06-27 11:12:06 +0800

    Update towerposition.cpp

--- ZLZ/tower1/towerposition.cpp
+++ ZLZ/tower1/towerposition.cpp
@@ -1,16 +1,21 @@
 #include "towerposition.h"
+#include "selectbutton.h"
 
 #include <QSize>
 #include <QPainter>
 #include <QPixmap>
 
 const QSize TowerPosition::m_fixedSize(35,35);
-TowerPosition::TowerPosition(QPoint pos,QString path)
-{
-    m_pos=pos;
-    m_path=path;
-    m_hasTower=0;
-}
+TowerPosition::TowerPosition(QPoint pos,QPixmap & sprite):
+    m_hasTower(false),
+    m_hasTower1(false),
+    m_hasTower2(false),
+    m_hasTower3(false),
+    m_hasButton(false),
+    m_hasButton2(false),
+    m_pos(pos),
+    m_sprite(sprite)
+    {}
 
 bool TowerPosition::hasTower()
 {
@@ -26,10 +31,21 @@ bool TowerPosition::hasTower1()
 {
     return m_hasTower1;
 }
+void TowerPosition::sethasTower1(bool x)
+{
+    m_hasTower1=x;
+    setHasTower(x);
+}
+
+bool TowerPosition::hasTower2()
+{
+    return m_hasTower2;
+}
 
 void TowerPosition::sethasTower2(bool x)
 {
     m_hasTower2=x;
+    setHasTower(x);
 }
 
 bool TowerPosition::hasTower3()
@@ -40,6 +56,7 @@ bool TowerPosition::hasTower3()
 void TowerPosition::sethasTower3(bool x)
 {
     m_hasTower3=x;
+    setHasTower(x);
 }
 
 bool TowerPosition::hasButton()
@@ -119,5 +136,16 @@ bool TowerPosition::ContainPos(QPoint pos)
 
 void TowerPosition::draw(QPainter* painter)const
 {
-    painter->drawPixmap(m_pos.x(),m_pos.y(),m_path);
+    painter->drawPixmap(m_pos.x(),m_pos.y(),m_sprite);
+}
+
+void TowerPosition::setRemoveTower()
+{
+    setTower(NULL);
+    setHasTower(false);
+    setHasTower1(false);
+    setHasTower2(false);
+    setHasTower3(false);
+    setHasUpdate1(false);
+    setHasUpdate2(false);
 }
