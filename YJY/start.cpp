412cc3de06b0b70dc410d9d4d25747e0f1aec827
Intersteller_Jasper <93512741+interstellarH@users.noreply.github.com>
2022-06-27 20:05:46 +0800

    Create start.cpp

---
+++ YJY/start.cpp
@@ -0,0 +1,60 @@
+#include "start.h"
+#include "./ui_start.h"
+#include "mainwindow.h"
+#include <QWindow>
+#include <QPainter>
+#include <QSize>
+#include <QString>
+
+
+start::start(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::start)
+{
+    ui->setupUi(this);
+    this->resize(QSize(500,500));
+    ui->label->setText("家瑛今天吃什么");
+}
+
+start::~start()
+{
+    delete ui;
+}
+
+void start::paintEvent(QPaintEvent* event)
+{
+    QPainter painter(this);
+    QString path(":/new/prefix1/resource1/20220527222135.jpg");
+
+    painter.drawPixmap(0,0,this->width(),this->height(),path);
+}
+
+void start::on_pushButton_clicked()
+{
+    MainWindow* first=new MainWindow(1);
+    first->setpath(":/new/prefix1/resource1/20220615141656.jpg");
+    first->setWindowTitle("第一关 程设期中考试");
+    first->show();
+    first->addWayPoint1();
+}
+
+
+void start::on_pushButton_2_clicked()
+{
+    MainWindow* second=new MainWindow(2);
+    second->setpath(":/new/prefix1/resource1/20220527194453.jpg");
+    second->setWindowTitle("第二关 魔兽世界大作业");
+    second->show();
+    second->addWayPoint2();
+}
+
+
+void start::on_pushButton_3_clicked()
+{
+    MainWindow* third=new MainWindow(3);
+    third->setpath(":/new/prefix1/resource1/20220527194528.jpg");
+    third->setWindowTitle("第三关 QT大作业");
+    third->show();
+    third->addWayPoint3();
+}
+
