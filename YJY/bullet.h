8a39bcacb31f762fd8f6b0f2edd8c5f084232cf0
En-2863 <100297679+En-2863@users.noreply.github.com>
2022-06-29 01:04:40 +0800

    Update bullet.h

--- YJY/bullet.h
+++ YJY/bullet.h
@@ -21,7 +21,7 @@ class Bullet :public QObject
     Q_PROPERTY(QPoint m_currentPos READ getCurrentPos WRITE setCurrentPos)//子弹动态移动
 public:
     Bullet();
-    Bullet(QPoint startPos,QPoint targetPos,Defend_Tower* tower,MainWindow * game,int damage = 0, int magical = 0);
+    Bullet(QPoint startPos,QPoint targetPos,Defend_Tower* tower,MainWindow * game,int damage=0, int magical=0);
     QPoint getCurrentPos();//得到子弹的当前位置
     void setCurrentPos(QPoint pos);//设置子弹的当前位置
 
@@ -38,14 +38,14 @@ protected:
 
     QPixmap b_sprite;//图片路径
 
-    QList<Enemy *> b_targetEnemy;//与defend_tower中定义一致 第一个位置代表攻击中心点的敌人
+    Enemy * b_targetEnemy;//与defend_tower中定义一致 第一个位置代表攻击中心点的敌人
                                  //单体攻击则size为1，群体攻击则包含所有可攻击到的目标
     Defend_Tower* b_tower;
     MainWindow * b_game;
 
     int b_damage = 0;//物理攻击值，与防御塔一致,应该保持在敌人血量的
                      //保证每秒对单个敌人的攻击达到普通敌人血线的50% 预设值在10-50之间
-    int b_magical = 0;//魔法攻击值，与防御塔一致
+    double b_magical = 0;//魔法攻击值，与防御塔一致
                       //保证每秒对单个敌人的攻击达到普通敌人血线的50% 预设值在10-50之间
     static const QSize m_fixedSize;
 };
@@ -105,14 +105,16 @@ class Frozen_Bullet:public Bullet{
 public:
     Frozen_Bullet(QPoint startpos,Defend_Tower* tower,MainWindow * game,int time);
     virtual void move();//实现冻住的函数
-    void draw_Frozen(QPainter * painter)const;//画出冰冻效果
+    void draw_Frozen(QPainter * painter);//画出冰冻效果
 
 private slots:
     virtual void hitTarget();
+    void reset_speed();
 
 private:
     int b_FrozenTime;//定住的时长
     int b_range;//定住的范围
     int b_CurTime = 0;
+    QList<Enemy*> enemylist;
 };
 #endif // BULLET_H
