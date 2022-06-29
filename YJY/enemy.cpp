6cb0f7e51c136f74abcb0fed8af3ba37c5420d0c
Intersteller_Jasper <93512741+interstellarH@users.noreply.github.com>
2022-06-29 13:33:42 +0800

    Update enemy.cpp

--- YJY/enemy.cpp
+++ YJY/enemy.cpp
@@ -19,13 +19,25 @@ Enemy::Enemy(wayPoint * startPoint, MainWindow * game,int type):
     e_type(type),
     e_pos(startPoint->getPos())
 {
-    e_maxHp=40;
-    e_currentHp=e_maxHp;
-    e_speed=1;
     e_active=false;
+    if(type==1){
+        e_currentHp=e_maxHp=40;
+        e_speed=1;
+        e_sprite=QPixmap(":/new/prefix1/resource1/M1.png");
+    }
+    else if(type==2){//血更厚但是速度慢
+        e_currentHp=e_maxHp=80;
+        e_speed=0.6;
+        e_sprite=QPixmap(":/new/prefix1/resource1/M2.png");
+    }
+    else if(type==3){//速度快血薄
+        e_currentHp=e_maxHp=15;
+        e_speed=3;
+        e_sprite=QPixmap(":/new/prefix1/resource1/M3.png");
+    }
     e_destinationWayPoint=startPoint->getNextWayPoint();
-    e_sprite=":/resources/monster1.png";
 }
+
 Enemy::~Enemy()
 {
     e_attackerTowerList.clear();
